231a53c411e7a1ad5364bbcebe46ea7f17346554
Nicholas Abboud <nicholas.abboud@ama.ab.ca>
2026-02-11 16:19:45 -0700

    update the global type file

--- global.d.ts
+++ global.d.ts
@@ -2,8 +2,7 @@ declare module '@amaabca/sensitive-param-filter' {
   export const SPFDefaultParams: string[]
   export const SPFDefaultReplacement: string
   export class SensitiveParamFilter {
-    constructor(options?: { params?: string[], replacement?: string })
-    filterObject<T>(obj: T): T
-    filterString(str: string): string
+    constructor(options?: { params?: string[], replacement?: string, filterUnknown?: boolean, whiteList?: string[] })
+    filter<T>(obj: T): T
   }
 }
