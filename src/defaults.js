cdab607da826fc1008cfdd8ad4ac987a763471d0
Nicholas Abboud <nicholas.abboud@ama.ab.ca>
2026-02-11 17:08:03 -0700

    removed the npm publish workflow that no longer works

--- src/defaults.js
+++ src/defaults.js
@@ -1,14 +1,4 @@
-const DEFAULT_PARAMS = [
-  'auth',
-  'bearer',
-  'CVD',
-  'CVV',
-  'encrypt',
-  'PAN',
-  'pass',
-  'secret',
-  'token',
-]
+const DEFAULT_PARAMS = ['auth', 'bearer', 'CVD', 'CVV', 'encrypt', 'PAN', 'pass', 'secret', 'token']
 
 const DEFAULT_REPLACEMENT = 'FILTERED'
 
